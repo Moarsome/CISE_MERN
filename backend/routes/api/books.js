3b0314f14d859c23f0af1c9f20aa275a93fe0b5f
Kylie Crump <moarsomecoarsome@gmail.com>
2023-08-10 20:58:11 +1200

    Fixed name error

--- backend/routes/api/books.js
+++ backend/routes/api/books.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Book = require('../../models/Book');
 
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
+router.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
