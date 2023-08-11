8501a9c25ea8dc6213b3297923e95badbacb9f60
Kylie Crump <moarsomecoarsome@gmail.com>
2023-08-11 19:17:06 +1200

    Updated backend url

--- my-app/src/components/UpdateBookInfo.js
+++ my-app/src/components/UpdateBookInfo.js
@@ -18,7 +18,7 @@ function UpdateBookInfo(props) {
 
   useEffect(() => {
     axios
-      .get(`https://cise-mern-backend-bk78d57i9-moarsome.vercel.app/api/books/${id}`)
+      .get(`https://cise-mern-backend.vercel.app/api/books/${id}`)
       .then((res) => {
         setBook({
           title: res.data.title,
@@ -51,7 +51,7 @@ function UpdateBookInfo(props) {
     };
 
     axios
-      .put(`https://cise-mern-backend-bk78d57i9-moarsome.vercel.app/api/books/${id}`, data)
+      .put(`https://cise-mern-backend.vercel.app/api/books/${id}`, data)
       .then((res) => {
         navigate(`/show-book/${id}`);
       })
